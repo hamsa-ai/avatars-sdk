00d7e837b0f5aa2852b1280cd7d1a914d6d2af19
Noor Bayari <nooraldeen.bayari@nadsoft.net>
2024-10-13 11:12:06 +0300

    update avatars configs

--- src/avatarsData.ts
+++ src/avatarsData.ts
@@ -1,38 +1,24 @@
 import type {AvatarConfig} from './utils/Types';
 
-// TODO: HOST VIDEOS ON AWS S3 OR OTHER STORAGE SERVICE AND UPDATE PATHS (100MB PLUS)
-
 export const avatarsSDKData: Record<string, AvatarConfig> = {
+  john: {
+    listeningSections: [{end: 45, start: 26}], // 00:26 - 00:45
+    speakingSections: [{end: 26, start: 0}], // 00:00 - 00:26
+    videoPath: 'https://hamsa-public.s3.eu-west-1.amazonaws.com/avatar/john.mp4',
+  },
   khalid: {
-    listeningSections: [{end: 21.2, start: 11.21}], // 11:13 - 21:12
-    speakingSections: [
-      {end: 6.27, start: 0}, // 00:00 - 06:16
-      {end: 11.21, start: 6.27}, // 06:16 - 11:13
-    ],
-    videoPath: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+    listeningSections: [{end: 43, start: 23}], // 00:23 - 00:43
+    speakingSections: [{end: 23, start: 0}], // 00:00 - 00:23
+    videoPath: 'https://hamsa-public.s3.eu-west-1.amazonaws.com/avatar/khalid.mp4',
   },
   layla: {
-    listeningSections: [{end: 20.2, start: 12.12}], // 12:07 - 20:12
-    speakingSections: [
-      {end: 6.1, start: 0}, // 00:00 - 06:06
-      {end: 12.12, start: 6.1}, // 06:06 - 12:07
-    ],
-    videoPath: 'avatar4Video',
+    listeningSections: [{end: 60, start: 39}], // 00:39 - 01:00
+    speakingSections: [{end: 39, start: 0}], // 00:00 - 00:39
+    videoPath: 'https://hamsa-public.s3.eu-west-1.amazonaws.com/avatar/layla.mp4',
   },
-  leo: {
-    listeningSections: [{end: 14.16, start: 9.08}], // 09:04 - 14:10
-    speakingSections: [
-      {end: 5.2, start: 0}, // 00:00 - 05:12
-      {end: 9.08, start: 5.2}, // 05:12 - 09:04
-    ],
-    videoPath: 'avatar1Video',
-  },
-  sophia: {
-    listeningSections: [{end: 14.16, start: 9.08}], // 09:04 - 14:10
-    speakingSections: [
-      {end: 5.2, start: 0}, // 00:00 - 05:12
-      {end: 9.08, start: 5.2}, // 05:12 - 09:04
-    ],
-    videoPath: 'avatar2Video',
+  natali: {
+    listeningSections: [{end: 67, start: 39}], // 00:39 - 01:07
+    speakingSections: [{end: 39, start: 0}], // 00:00 - 00:39
+    videoPath: 'https://hamsa-public.s3.eu-west-1.amazonaws.com/avatar/natali.mp4',
   },
 };
