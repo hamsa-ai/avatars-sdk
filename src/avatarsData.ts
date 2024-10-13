cb37e0fe8c99622e04c1968cbf7902867b6127b9
Noor Bayari <nooraldeen.bayari@nadsoft.net>
2024-10-13 14:18:24 +0300

    update avatar data

--- src/avatarsData.ts
+++ src/avatarsData.ts
@@ -2,22 +2,22 @@ import type {AvatarConfig} from './utils/Types';
 
 export const avatarsSDKData: Record<string, AvatarConfig> = {
   john: {
-    listeningSections: [{end: 45, start: 26}], // 00:26 - 00:45
+    listeningSections: [{end: 45, start: 28}], // 00:28 - 00:45
     speakingSections: [{end: 26, start: 0}], // 00:00 - 00:26
     videoPath: 'https://hamsa-public.s3.eu-west-1.amazonaws.com/avatar/john.mp4',
   },
   khalid: {
-    listeningSections: [{end: 43, start: 23}], // 00:23 - 00:43
+    listeningSections: [{end: 43, start: 25}], // 00:25 - 00:43
     speakingSections: [{end: 23, start: 0}], // 00:00 - 00:23
     videoPath: 'https://hamsa-public.s3.eu-west-1.amazonaws.com/avatar/khalid.mp4',
   },
   layla: {
-    listeningSections: [{end: 60, start: 39}], // 00:39 - 01:00
+    listeningSections: [{end: 60, start: 45}], // 00:45 - 01:00
     speakingSections: [{end: 39, start: 0}], // 00:00 - 00:39
     videoPath: 'https://hamsa-public.s3.eu-west-1.amazonaws.com/avatar/layla.mp4',
   },
   natali: {
-    listeningSections: [{end: 67, start: 39}], // 00:39 - 01:07
+    listeningSections: [{end: 67, start: 48}], // 00:48 - 01:07
     speakingSections: [{end: 39, start: 0}], // 00:00 - 00:39
     videoPath: 'https://hamsa-public.s3.eu-west-1.amazonaws.com/avatar/natali.mp4',
   },
